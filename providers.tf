b84cee706f6257cda73d3f9df19ef79b68f5fa68
patrick-sullivan-dev <48216540+patrick-sullivan-dev@users.noreply.github.com>
2026-08-01 11:24:00 -0400

    refactor!: align virtual machine inputs with provider

--- providers.tf
+++ providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_version = ">= 1.13.5"
   required_providers {
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = ">= 2.4.0"
     }
     proxmox = {
-      source = "bpg/proxmox"
-      version = ">= 0.87.0"
+      source  = "bpg/proxmox"
+      version = ">= 0.111.1"
     }
     macaddress = {
-      source = "ivoronin/macaddress"
+      source  = "ivoronin/macaddress"
       version = ">= 0.3.2"
     }
   }
